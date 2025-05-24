6e426381b47376509421757290517214af19ad5e
Ahmad-Amireh <ahmadamerh19@gmail.com>
2025-05-24 17:20:08 +0300

    Add data router

---
+++ src/routes/data.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Depends, APIRouter, UploadFile 
+from helpers.config import get_settings, Settings 
+from controllers import DataController
+
+data_router = APIRouter(
+    prefix = "/api/v1/data",
+    tags =["api_v1","data"]
+)
+
+@data_router.post("/upload/{project_id}") #for each tenant
+async def upload_data(project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)):
+    
+    #validate the file properties
+    is_valid = DataController().validate_upload_file(file= file)
+    return is_valid
