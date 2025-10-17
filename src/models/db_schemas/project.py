82939be90999007467a747d993433d7e1c7e2480
Ahmad-Amireh <ahmadamerh19@gmail.com>
2025-10-17 14:47:56 +0300

    Add project model

---
+++ src/models/db_schemas/project.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional
+from bson import ObjectId
+
+class Project(BaseModel):
+    id: Optional[ObjectId]
+    project_id: str = Field(..., min_length=1)
+
+    @field_validator("project_id")
+    @classmethod  # it's on calss level not object
+    def validate_project_id(cls, value: str) -> str:
+        if not value.isalnum():
+            raise ValueError('project_id must be alphanumeric')
+        return value
+    
+    class Config:
+        arbitrary_types_allowed = True # to allow not pydantic 
